f7ed9abd3467f23818935427df346a23888d842e
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-18 19:41:10 +0300

    Added NavigationController, added second ViewController "Add List", added Bar Button Item "Add" on MainVC

---
+++ sticky/sticky/AddListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddListViewController.swift
+//  sticky
+//
+//  Created by Artsiom Sadyryn on 1/18/18.
+//  Copyright © 2018 Artsiom Sadyryn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddListViewController: UIViewController {
+    
+}
