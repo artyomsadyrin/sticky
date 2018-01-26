a5352ff62dc40d4f2e517eb1ea9f6c23c2099a5f
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-26 21:08:14 +0300

    Generated classes from CoreData

---
+++ sticky/CoreData/List+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  List+CoreDataClass.swift
+//  sticky
+//
+//  Created by Artsiom Sadyryn on 1/26/18.
+//  Copyright © 2018 Artsiom Sadyryn. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(List)
+public class List: NSManagedObject {
+
+}
