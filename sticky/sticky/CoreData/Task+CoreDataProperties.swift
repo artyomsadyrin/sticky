bc43554952448b1c5a2f812420b61cc820970c22
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-27 11:04:39 +0300

    Created NSManagedObject subclass for CoreData entities

---
+++ sticky/sticky/CoreData/Task+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Task+CoreDataProperties.swift
+//  sticky
+//
+//  Created by Artsiom Sadyryn on 1/27/18.
+//  Copyright © 2018 Artsiom Sadyryn. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var descriptionTask: String?
+    @NSManaged public var note: String?
+    @NSManaged public var time: NSDate?
+    @NSManaged public var list: List?
+
+}
