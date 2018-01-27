57506e1152e705415b2c4030c1393cb665ee275c
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-27 14:32:46 +0300

    Finally getting working copy to branch 'master'

---
+++ sticky/sticky/Model/Lists.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  sticky
+//
+//  Created by Artsiom Sadyryn on 1/23/18.
+//  Copyright © 2018 Artsiom Sadyryn. All rights reserved.
+//
+
+import Foundation
+
+class Lists {
+    var name: String
+    //var textColor: String
+    
+    init?(name: String) {
+        self.name = name
+    }
+}
